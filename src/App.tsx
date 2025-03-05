ac88b6899953f59c5535b339e6b2929f77339253
dust1-str <angel.t069@outlook.com>
2025-03-04 18:56:14 -0600

    Add tracker

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './screens/home/home'
 import ConfirmAccount from './screens/confirm-account/confirm-account'
+import Tracker from './screens/tracker/tracker'
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route element={<Home/>} path='/home'></Route>
         <Route element={<ConfirmAccount/>} path='/confirm-account'></Route>
+        <Route element={<Tracker/>} path='/tracker'></Route>
       </Routes>
     </BrowserRouter>
   )
