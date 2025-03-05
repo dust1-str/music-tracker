121a46420b0581f68527a4d79e41b25d34b061c2
dust1-str <angel.t069@outlook.com>
2025-03-04 18:48:44 -0600

    Add current song display

--- src/screens/confirm-account/confirm-account.tsx
+++ src/screens/confirm-account/confirm-account.tsx
@@ -1,9 +1,36 @@
+import { useLocation } from 'react-router-dom'
 import('../../App.css')
 
 export default function ConfirmAccount() {
+    const location = useLocation()
+    const tracks = location.state?.tracks
+    const recentTrack = tracks.recenttracks.track[0]
+    const artist = recentTrack.artist['#text']
+    const song = recentTrack.name
+    const image = recentTrack.image[2]['#text']
+    
     return (
         <>
-            <h1>Hola</h1>
+            <div className='flex items-center justify-center min-h-screen bg-gray-50'>
+                <div className="p-4 items-center justify-center w-[680px] rounded-xl group sm:flex space-x-6 bg-white shadow-xl hover:rounded-2xl">
+                    <img  className="mx-auto size-auto rounded-lg"  alt="art cover" loading="lazy" src={image} />
+                    <div className="sm:w-8/12 pl-0 p-5">
+                        <div className="space-y-2">
+                            <div className="space-y-4">
+                                <h4 className="text-lg font-semibold text-cyan-900 text-justify">
+                                    {song}
+                                </h4>
+                                <h4 className="text-lg font-semibold text-cyan-900 text-justify">
+                                    {artist}
+                                </h4>
+                                <p><strong>Is this the last song you heard, or are you listening to it right now?</strong></p>
+                                <button className="mr-5 bg-green-800 px-10 py-3 text-white rounded-2xl hover:bg-green-900">Yes</button>
+                                <button className="mr-5 bg-red-600 px-10 py-3 text-white rounded-2xl hover:bg-red-700">No</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,38 +1,45 @@
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import '../../App.css'
 
 export default function Home() {
+  const [pass, setPass] = useState(false)
+  const [tracks, setTracks] = useState(null)
+  const API_KEY = "850027ec29d021d82cceb55d8d29b65c"
+  const navigate = useNavigate();
+
   async function validateData(formData: FormData) {
     'use server'
-    const API_KEY = formData.get('api-key')
     const USER = formData.get('user')
-    if (API_KEY && USER) {
+    if (USER) {
       const URL = `http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=${USER}&api_key=${API_KEY}&format=json&limit=1`
-      console.log(URL)
       await fetch(URL)
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => {
+          setTracks(data)
+          setPass(true)
+        })
     } else {
       console.error("Key is null")
     }
   }
+
+  useEffect(() => {
+    if (pass && tracks)
+      navigate('/confirm-account', { state: { tracks } })
+  }, [pass, tracks])
 
 
   return (
     <>
       <div className="flex h-screen w-screen flex-col justify-center items-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h3 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Hi! Thank you for using my music tracker. Before you get started, please type your lastFM key below</h3>
+          <h3 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Hi! Thank you for using my music tracker. Before you get started, please type your lastFM user below</h3>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" action={validateData} method="POST">
             <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="api-key" className="block text-sm/6 font-medium text-gray-900">Your lastFM key</label>
-              </div>
-              <div className="mt-2">
-                <input type="text" name="api-key" id="api-key" required className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"/>
-              </div>
               <div className="flex items-center justify-between">
                 <label htmlFor="user" className="block text-sm/6 font-medium text-gray-900">Your lastFM user</label>
               </div>
